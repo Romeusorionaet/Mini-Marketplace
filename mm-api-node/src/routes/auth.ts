8cc9322efc5f1aa6ee419e37e61bb21ca4c6af19
Romeu Sorionaet <romeusoares14569@gmail.com>
2025-09-27 18:03:26 -0300

    refactor-api: renamed file from 'auth-register' to 'auth'

---
+++ mm-api-node/src/routes/auth.ts
@@ -0,0 +1,49 @@
+import { FastifyInstance } from "fastify";
+import { database } from "src/database/db";
+import { providers, users } from "src/database/schemas";
+import { hashPassword } from "src/utils/cryptography";
+
+export async function registerRoutes(app: FastifyInstance) {
+  app.post<{ Body: RegisterAuthBodyType }>(
+    "/register",
+    async (request, reply) => {
+      const userData = request.body;
+
+      const hashedPassword = await hashPassword(userData.password);
+
+      try {
+        const [userCreated] = await database
+          .insert(users)
+          .values({
+            name: userData.name,
+            email: userData.email,
+            passwordHash: hashedPassword,
+            role: userData.role,
+            city: userData.city,
+            createdAt: new Date(),
+            updatedAt: new Date(),
+          })
+          .returning();
+
+        if (userData.role === "PROVIDER") {
+          await database.insert(providers).values({
+            userId: userCreated.id,
+            city: userData.city,
+          });
+        }
+
+        return reply
+          .status(201)
+          .send({ message: "Usuário registrado com sucesso" });
+      } catch (err: any) {
+        const pgError = err.cause ?? err;
+
+        if (pgError.code === "23505") {
+          return reply.status(400).send({ message: "Email já cadastrado" });
+        }
+
+        return reply.status(500).send({ message: "Erro interno do servidor" });
+      }
+    }
+  );
+}
