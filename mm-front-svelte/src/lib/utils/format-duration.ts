a94d080416fabaa474fa3530debc4b61fd24b986
Romeu Sorionaet <romeusoares14569@gmail.com>
2025-09-30 10:48:51 -0300

    refactor-front: separated fetch api logic, created an auth store and a utils for formate duration time

---
+++ mm-front-svelte/src/lib/utils/format-duration.ts
@@ -0,0 +1,6 @@
+export function formatDuration(minutes: number) {
+  if (minutes < 60) return `${minutes} min`;
+  const hours = Math.floor(minutes / 60);
+  const mins = minutes % 60;
+  return mins > 0 ? `${hours}h ${mins}min` : `${hours}h`;
+}
